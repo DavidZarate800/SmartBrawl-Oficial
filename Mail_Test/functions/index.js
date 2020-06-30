c6f36bfe8b1b9384c7c5d16f418fe0731538ae58
Ricardo <ricardo.carrillo.villalpando@gmail.com>
2020-06-29 22:49:22 -0500

    Mail en hosting (test)
    Prueba.

--- Mail_Test/functions/index.js
+++ Mail_Test/functions/index.js
@@ -1,12 +1,23 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const { response } = require('express');
-const cors = require('cors');
-const app =express();
+const functions = require("firebase-functions");
+const express = require("express");
+const { response } = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const configMensaje = require("./configMensaje");
+
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/api',(req,res)=>{
-    res.json("hola mundo desde Node JS!");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
+  res.json("hola mundo desde Node JS!");
+});
+
+app.post("/form", (req, res) => {
+  configMensaje(req.body);
+  res.status(200).send();
 });
 
 // // Create and Deploy Your First Cloud Functions
